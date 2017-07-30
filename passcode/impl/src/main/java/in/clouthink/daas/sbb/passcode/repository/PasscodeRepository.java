26605d6dc4071f09180c8bfaa0b116235c1c1381
dz <melthaw@gmail.com>
2017-07-30 19:48:42 +0800

    refactor passcode module

--- passcode/impl/src/main/java/in/clouthink/daas/sbb/passcode/repository/PasscodeRepository.java
+++ passcode/impl/src/main/java/in/clouthink/daas/sbb/passcode/repository/PasscodeRepository.java
@@ -3,10 +3,9 @@ package in.clouthink.daas.sbb.passcode.repository;
 
 import in.clouthink.daas.sbb.passcode.model.Passcode;
 import in.clouthink.daas.sbb.shared.repository.AbstractRepository;
+import org.springframework.stereotype.Repository;
 
-/**
- * @author dz
- */
+@Repository
 public interface PasscodeRepository extends AbstractRepository<Passcode> {
 
 	Passcode findByCellphoneAndCategory(String cellphone, String category);
