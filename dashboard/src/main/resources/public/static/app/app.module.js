6126e9398d3476c9c95eb0e5cb0314dfa8fe0c51
dz <melthaw@gmail.com>
2017-07-30 18:28:46 +0800

    remove unused model

--- dashboard/src/main/resources/public/static/app/app.module.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('webapp', ['ui.router', 'ui.bootstrap', 'ui.event',
-    'ui.uploader', 'dialogs.main',
-    'angular-noty', 'ngAnimate', 'ngCkeditor',
-    'treeControl', 'AngularPrint', 'ncy-angular-breadcrumb', 'angular-clipboard']);
-
-
