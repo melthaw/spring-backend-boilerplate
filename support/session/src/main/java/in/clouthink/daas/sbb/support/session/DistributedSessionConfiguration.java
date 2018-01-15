786016f64aa525bceed101fb97aa9d9dd501fdbf
chenxue <melthaw@gmail.com>
2018-01-15 12:17:37 +0800

    add new module (:support/session)

---
+++ support/session/src/main/java/in/clouthink/daas/sbb/support/session/DistributedSessionConfiguration.java
@@ -0,0 +1,11 @@
+package in.clouthink.daas.sbb.support.session;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ */
+@Configuration
+public class DistributedSessionConfiguration {
+
+}
